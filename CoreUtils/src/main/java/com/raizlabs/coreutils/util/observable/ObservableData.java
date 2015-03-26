0004f8433466efd8b2d1d9443fb46619918f2d16
Andrew Grosner <andrew.grosner@raizlabs.com>
2015-03-26 12:27:08 -0400

    initial commit for coreutils

---
+++ CoreUtils/src/main/java/com/raizlabs/coreutils/util/observable/ObservableData.java
@@ -0,0 +1,18 @@
+package com.raizlabs.coreutils.util.observable;
+
+import com.raizlabs.coreutils.events.Event;
+
+/**
+ * Interface which indicates that a class provides an {@link Event} which is
+ * raised when its data changes.
+ * 
+ * @param <T> The type of data that will be sent as the arguments to the data
+ * change event. In many cases, this may just be the class itself.
+ */
+public interface ObservableData<T> {
+	/**
+	 * @return The {@link Event} which will be raised when the data in this
+	 * object changes.
+	 */
+	public Event<T> getDataChangedEvent();
+}
